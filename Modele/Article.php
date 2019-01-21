dee83e76fb006d3216c5022d6a71289fca324050
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-01-21 23:29:21 +0100

    no message

--- Modele/Article.php
+++ Modele/Article.php
@@ -1,7 +1,7 @@
 <?php
 
 // POPO article qui stockera les données relatives à un article
-//Les objets de ce projet sont les classes: articles, commentaires, utilisateur
+//Les objets de ce projet sont les classes: articles, commentaires, utilisateurs
 // Il se compose de getters (accesseurs) qui permettront l'accès aux attributs et de setters (mutateurs) qui permettront
 // de changer les valeurs des attributs.
 // L'hydratation de l'objet se fera via un array avec les données correspondantes aux attributs.
