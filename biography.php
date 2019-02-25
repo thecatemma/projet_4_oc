909920429f8e5bbc222b5c4750f448ef62375043
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-02-25 22:38:41 +0100

    Ajout de la page biographie

---
+++ biography.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Biographie></title>
+	</head>
+	
+		
+		
+	<nav class="navigator">
+	  <div id="menuToggle">
+		    <input type="checkbox"/>
+				<span></span>
+			    <span></span>
+			    <span></span>	
+			<ul id="nav">
+				  <li><a href="index.php">Home</a>
+				  <li><a href="article.php?id=1">Livre</a>
+				  <li><a href="biography.php">Biographie</a>
+				  <li><a href="index.php?action=connexion">Admin</a>
+		    </ul>
+		 </div>
+	   </nav>
+
+	
+<body>	
+	
+	  <div class="container">
+		  
+		  
+		  <p>Aujourd'hui il semble que chacun, célèbre ou non, souhaite laisser une trace : en témoigne le développement d'entreprises spécialisées, proposant aux particuliers de réaliser leur propre biographie, pour conserver la mémoire de leur vie, dans un but privé ou public. Il s'agit de « films de vie » ou « biographies filmées » ou encore de récits de vie écrits par des écrivains publics, conseils en écriture ou biographes. L’impartialité, la véracité même du récit n’ont plus ici aucune importance : son personnage souhaite voir transcrit le recueil de sa seule parole, donc la vision subjective et personnelle de son histoire. Cette prétention à la subjectivité se retrouve aussi dans la biographie romancée, ou exofiction, en vogue en France depuis 2015.</p>
+		
+
+		</div>
+	
+</body>
+</html>
