d36af4139a48e14854f31a6e626c6ce7374f2978
Emmanuelle Husson <husson.emmanuelle.miou@gmail.com>
2019-03-30 22:50:03 +0100

    lecture

--- View/View.php
+++ View/View.php
@@ -1,51 +1,46 @@
 <?php
 
 //View: affiche les informations dans une page html
-
 //cette class permet de créer un objet, qui contient des variables
-//accès aux éléments privés est uniquement réservé à la classe qui les a définie
+//l'accès aux éléments privés est uniquement réservé à la classe qui les a défini
 //http://php.net/manual/fr/language.oop5.visibility.php?fbclid=IwAR3NbLa_Ae4ATjD662LsR1ueJkHKMMFgLxj0HXz1F4rJVpOKsK90n8jpnsQ
-
 // une class sert à construire un objet
 
 class View {
-
 // Nom du fichier associé à la vue
   private $fichier; 
 // Titre de la vue (défini dans le fichier vue)
   private $titre;
 
   public function __construct($action) {
-// les 2 tirets se mettent toujours devant le constructeur
-//class appelée quand on appelle l'objet, qu'est-ce que ça fait ?
-//le constructeur méthode par défaut quand la class et appelée
-// Détermination du nom du fichier vue à partir de l'action
+//Les 2 tirets se mettent toujours devant le constructeur
+//Class appelée quand on appelle l'objet, qu'est-ce que ça fait ?
+//Le constructeur méthode par défaut quand la class et appelée
+//Détermination du nom du fichier vue à partir de l'action
     $this->fichier = "view/view" . $action . ".php";
-//ça instancie, la prend la ce fichier est égal à view view .php
+//ça instancie, la prend la ce fichier est égal à view view.php
   }
-
-// Génère et affiche la vue
+//Génère et affiche la vue
   public function generer($donnees) {
-    // Génération de la partie spécifique de la vue
+//Génération de la partie spécifique de la vue
     $contenu = $this->genererFichier($this->fichier, $donnees);
- // Génération du gabarit commun utilisant la partie spécifique
+//Génération du gabarit commun utilisant la partie spécifique
     $view = $this->genererFichier('view/template.php',
       array('titre' => $this->titre, 'contenu' => $contenu));
-// Renvoi de la view au navigateur
+//Renvoi de la view au navigateur
     echo $view;
   }
-
-// Génère un fichier vue et renvoie le résultat produit
+//Génère un fichier vue et renvoie le résultat produit
   private function genererFichier($fichier, $donnees) {
     if (file_exists($fichier) AND $donnees) {
-// Rend les éléments du tableau $donnees accessibles dans la vue
+//Rend les éléments du tableau $donnees accessibles dans la vue
       extract($donnees);
-// Démarrage de la temporisation de sortie
+//Démarrage de la temporisation de sortie
       ob_start();
-// Inclut le fichier vue
-// Son résultat est placé dans le tampon de sortie
+//Inclut le fichier vue
+//Son résultat est placé dans le tampon de sortie
       require $fichier;
-// Arrêt de la temporisation et renvoi du tampon de sortie
+//Arrêt de la temporisation et renvoi du tampon de sortie
       return ob_get_clean();
     }
     else if (file_exists($fichier) AND !($donnees)) {
